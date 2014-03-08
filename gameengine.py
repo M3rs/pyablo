4e0eb5b266acf26fb7f0fdcf60b2213496c97fa9
Andy Moran <andy@Andy-Morans-MacBook.local>
2014-03-07 21:29:48 -0600

    'actually pushing changes?'

--- gameengine.py
+++ gameengine.py
@@ -2,41 +2,73 @@ import pygame
 from pygame.locals import *
 
 class GameEngine(object):
-""" Manages a stack of GameStates, runs game """
-	def __init__():
-		self.running = True
-		self.states = []
-		self.add_state(TitleState())
-	
-	def add_state(self, state):
-		""" Adds state to the end of states """
-		self.states.append(state):
-		self.states[-1].engine = self
-	
-	def switch_state(self, state):
-		""" Changes the top state to state """
-		self.states[-1] = state
-		self.states[-1] = self
+    """ Manages a stack of GameStates, runs game """
+    def __init__(self):
+        self.running = True
+        self.states = []
+        self.add_state(TitleState())
+    
+    def add_state(self, state):
+        """ Adds state to the end of states """
+        self.states.append(state)
+        self.states[-1].engine = self
+    
+    def switch_state(self, state):
+        """ Changes the top state to state """
+        self.states[-1] = state
+        self.states[-1] = self
 
-	def pop_state(self):
-		""" Pops the last state off """
-		self.states.pop()
+    def pop_state(self):
+        """ Pops the last state off """
+        self.states.pop()
 
-	def state(self):
-		return self.states[-1]
+    def state(self):
+        return self.states[-1]
 
 class GameState(object):
-	""" Base class for all game states """
-	def __init__(self):
-		pass
+    """ Base class for all game states """
+    def __init__(self):
+        pass
 
-	def render(self, screen):
-		raise NotImplementedError
+    def render(self, screen):
+        raise NotImplementedError
 
-	def update(self, events):
-		raise NotImplementedError
+    def update(self, events):
+        raise NotImplementedError
 
-	def handle_events(self, events):s
-		raise NotImplementedError
+    def handle_events(self, events):
+        raise NotImplementedError
 
-	
+class TitleState(object):
+    """ Main Title """
+    def __init__(self):
+        pass    
+
+    def render(self, screen):
+        raise NotImplementedError
+        
+    def update(self, events):
+        raise NotImplementedError
+
+    def handle_events(self, events):
+        """ Pressing any key moves to Menu State """
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                self.engine.running = False
+            else:
+                self.engine.switch_state(MenuState())
+        
+
+class MenuState(object):
+    """ Main Menu """
+    def __init__(self):
+        pass    
+
+    def render(self, screen):
+        raise NotImplementedError
+        
+    def update(self, events):
+        raise NotImplementedError
+
+    def handle_events(self, events):
+        raise NotImplementedError
